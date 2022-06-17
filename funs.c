25d20158807f2f02744590df2d1f4a56e4a7e833
saher <msaher.shair@gmail.com>
2022-06-17 09:47:26 +0300

    Add distributions functions

---
+++ funs.c
@@ -0,0 +1,17 @@
+#include <gsl/gsl_cdf.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+
+double  z_cdf(double               args[])  {  return  gsl_cdf_gaussian_P(args[0],          1);        }                     
+double  z_invcdf(double            args[])  {  return  gsl_cdf_gaussian_Pinv(args[0],       1);        }                     
+double  t_cdf(double               args[])  {  return  gsl_cdf_tdist_Q(args[0],             args[1]);  }                     
+double  t_invcdf(double            args[])  {  return  gsl_cdf_tdist_Qinv(args[0],          args[1]);  }                     
+/* double  exponential_cdf(double     args[])  {  return  gsl_cdf_exponential_P(args[0],       args[1]);  } */                     
+/* double  f_cdf(double               args[])  {  return  gsl_cdf_fdist_Q(args[0],             args[1],   args[2]);  } */          
+/* double  f_cdfinv(double            args[])  {  return  gsl_cdf_fdist_Qinv(args[0],          args[1],   args[2]);  } */          
+/* double  weibull_cdf(double         args[])  {  return  gsl_cdf_weibull_P(args[0],           args[1],   args[2]);  } */          
+/* double  poisson_pdf(double         args[])  {  return  gsl_ran_poisson_pdf(args[0],         args[1]);  } */                     
+/* double  poisson_cdf(double         args[])  {  return  gsl_cdf_poisson_P(args[0],           args[1]);  } */                     
+/* double  bionomial_pdf(double       args[])  {  return  gsl_ran_binomial_pdf(args[0],        args[1],   args[2]);  } */          
+/* double  geometric_pdf(double       args[])  {  return  gsl_ran_geometric_pdf(args[0],       args[1]);  } */                     
+/* double  hypergeometric_pdf(double  args[])  {  return  gsl_ran_hypergeometric_pdf(args[0],  args[1],   args[2],   args[3]);  } */
---
+++ funs.h
@@ -0,0 +1,39 @@
+#ifndef FUNS_H
+#define FUNS_H
+#define MAX_ARGS 4
+double  z_cdf(double               args[]);
+double  z_invcdf(double            args[]);
+
+double  t_cdf(double               args[]);
+double  t_invcdf(double            args[]);
+
+/* double  exponential_cdf(double     args[]); */
+
+/* double  f_cdf(double               args[]); */
+/* double  f_cdfinv(double            args[]); */
+
+/* double  weibull_cdf(double         args[]); */
+
+/* double  poisson_pdf(double         args[]); */
+/* double  poisson_cdf(double         args[]); */
+
+/* double  bionomial_pdf(double       args[]); */
+/* double  geometric_pdf(double       args[]); */
+
+/* double  hypergeometric_pdf(double  args[]); */
+
+
+/* double z_cdf(double z); */
+/* double z_invcdf(double z); */
+/* double (*t_cdf)(double, double); */
+/* double (*t_invcdf)(double, double); */
+/* double (*exponential_cdf)(double x, double mu); */
+/* double (*f_cdf)(double x, double nu1, double nu2); */
+/* double (*f_cdfinv)(double x, double nu1, double nu2); */
+/* double(*weibull_cdf)(double x, double a, double b); */
+/* double (*poisson_pdf)(unsigned int k, double mu); */
+/* double (*poisson_cdf)(unsigned int k, double mu); */
+/* double (*bionomial_pdf)(unsigned int k, double p, unsigned int n); */
+/* double (*geometric_pdf)(unsigned int k, double p); */
+/* double (*hypergeometric_pdf)(unsigned int k, unsigned int n1, unsigned int n2, unsigned int t); */
+#endif
