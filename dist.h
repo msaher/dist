8323f122f826421e877592e714c94b06b0221e57
saher <msaher.shair@gmail.com>
2022-06-17 11:47:59 +0300

    Refactor structs to take discrete dists and cdf direction into account

--- dist.h
+++ dist.h
@@ -1,10 +1,25 @@
 #ifndef DIST_H
 #define DIST_H
 
+/* probability function */
+typedef double (*)(double[]) Prob;
+
+typedef struct {
+    Prob left;
+    Prob right;
+} Cdfs;
+
 typedef struct {
     char name;
-    double (*funs[])(double[]);
-} Dist;
+    Cdfs cdfs;
+    Cdfs invcdfs;
+} Contenious;
+
+typedef struct {
+    char name;
+    Cdfs cdfs;
+    Prob pdf;
+} Discrete;
 
 extern Dist zdist;
 extern Dist tdist;
