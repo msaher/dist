2d0cf073cdfe1b646638feb695d9da3ab39046fa
saher <msaher.shair@gmail.com>
2022-06-22 21:16:22 +0300

    remove unused macro

--- utils.h
+++ utils.h
@@ -1,5 +1,4 @@
 #ifndef UTILS_h
 #define UTILS_h
 #define ERROR(...) fprintf(stderr, __VA_ARGS__), exit(1)
-#define ERROR_USAGE() ERROR("Usage: %s [-lir] [-z | -t] n1 [n2] [n3] [n4]\n", argv[0])
 #endif
